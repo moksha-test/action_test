6274db16168ce497297e44067b6fc3c72246f865
moksha-test <moksha.k11102@gmail.com>
2025-04-18 13:11:40 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -2,7 +2,7 @@ def is_palindrome(word):
     return word == word[::-1]
 
 if __name__ == "__main__":
-    word = "madam"  # You can change this word
+    word = "hello"  # You can change this word
     if is_palindrome(word):
         print(f"✅ {word} is a palindrome!")
     else:
